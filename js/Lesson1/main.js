6ceadf89a199e679c061446bd3834f522a0e4c88
AntonVarvianskyi <anton.varvianskyi@gmail.com>
2023-01-31 21:00:41 +0200

    homework1

---
+++ js/Lesson1/main.js
@@ -0,0 +1,36 @@
+let a = 'hello';
+let b = 'owu';
+let c = true;
+let d = false;
+let e = 4;
+let f = 5;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+let firstName = 'Anton';
+let middleName = 'Olegovych';
+let lastName = 'Varvianskyi';
+let con = `${firstName} ${middleName} ${lastName}`;
+console.log(con);
+
+let g = 100; let k = '100'; let m = true;
+
+console.log(typeof g, typeof k, typeof m)
+
+let name = prompt("Ваше імя'я:")
+let nameF = prompt("Ваше прізвище:")
+let Age = prompt("Ваш вік:")
+console.log(name, nameF, Age)
+
+
+
+
+
+
+
+
+
